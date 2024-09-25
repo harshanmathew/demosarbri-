cf119372bbc62e066fcb0e5c6829a17635850d97
Sachin Babu <sachinbabu.dev@gmail.com>
2024-09-26 01:19:52 +0530

    test

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,12 +10,12 @@ export class UsersService {
 	constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
 	async create(createUserDto: CreateUserDto): Promise<User> {
-		   const newUser = new this.userModel(createUserDto)
+		const newUser = new this.userModel(createUserDto)
 		return newUser.save()
 	}
 
 	async findAll(): Promise<User[]> {
-		   return this.userModel.find().exec()
+		return this.userModel.find().exec()
 	}
 
 	// Get a user by ID
